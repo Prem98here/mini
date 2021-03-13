453d966965ebd5bc69c3e27e6eb0bc69d2f38a5e
prem singh <prem98here@gmail.com>
2021-03-13 16:43:13 +0530

    initial commit

---
+++ social/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from social.models import Posts
+from social.models import Likes
+
+# Register your models here.
+
+admin.site.register(Posts)
+admin.site.register(Likes)
